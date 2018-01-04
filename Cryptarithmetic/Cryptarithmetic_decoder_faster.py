ff287f8288adab7cb69cecec28e6e6e474bc3cf8
chilinoodle <mazen426@gmail.com>
2018-01-04 12:22:10 +0100

    first commit

---
+++ Cryptarithmetic/Cryptarithmetic_decoder_faster.py
@@ -0,0 +1,29 @@
+# solve: runs through all possible combinations testing each for valid solution
+# fill_in: creates a new formula replacing letters with numbers
+# valid: tests out filled in string
+
+import re
+import string
+import itertools
+import cProfile
+
+
+def fast_solve(rawFormula):
+    pass
+
+
+def compile_formula(rawFormula):
+    """
+    Compile formula into a function.
+    :param rawFormula:
+    :return: the function as well as the letters used as an argument in the function
+    """
+    pass
+
+
+def compile_partition:
+    """
+    Compile a formula Partition of UPPER case letters as numeric digits (each word is a partition)
+    None uppercase formula partition to be unchanged
+    """
+    pass
