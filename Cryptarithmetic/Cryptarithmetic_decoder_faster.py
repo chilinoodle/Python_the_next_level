2a64440f941655d5b791470f1927cbb6cef10d2a
chilinoodle <mazen426@gmail.com>
2018-01-04 13:40:23 +0100

    final

--- Cryptarithmetic/Cryptarithmetic_decoder_faster.py
+++ Cryptarithmetic/Cryptarithmetic_decoder_faster.py
@@ -1,4 +1,3 @@
-
 import re
 import string
 import itertools
@@ -6,21 +5,46 @@ import cProfile
 
 
 def fast_solve(rawFormula):
-    pass
+    f, letters = compile_formula(rawFormula)
+    for digits in itertools.permutations((1, 2, 3, 4, 5, 6, 7, 8, 9, 0), len(letters)):
+        try:
+            if f(*digits) is True:
+                table = string.maketrans(letters, "".join(map(str, digits)))
+                candidate = rawFormula.translate(table)
+                if not re.search(r'\b0[0-9]', candidate):
+                    return candidate
+        except ArithmeticError:
+            pass
 
 
-def compile_formula(rawFormula):
+def compile_formula(rawFormula, debug = True):
     """
     Compile formula into a function.
-    :param rawFormula:
+    :param rawFormula, debug = True if we want to actually  print how does lambda look like
     :return: the function as well as the letters used as an argument in the function
     """
-    pass
+    letters = "".join(set(re.findall("[A-Z]", rawFormula)))
+    params = ", ".join(letters)
+    partitions = map(compile_partition, re.split("([A-Z]+)", rawFormula))
+    formula = "".join(partitions)
+    f = "lambda {}: {}".format(params, formula)
+    if debug: print f
+    return eval(f), letters
 
 
-def compile_partition:
+def compile_partition(formulaPartition):
     """
     Compile a formula Partition of UPPER case letters as numeric digits (each word is a partition)
     None uppercase formula partition to be unchanged
     """
-    pass
+    if formulaPartition.isupper():
+        terms = [('{}*{}'.format(10**i, d)) for i, d in enumerate(formulaPartition[::-1])]
+        return "("+"+".join(terms)+")"
+    else:
+        return formulaPartition
+
+
+question = "SEND + MORE == MONEY"
+
+print fast_solve(question)
+
